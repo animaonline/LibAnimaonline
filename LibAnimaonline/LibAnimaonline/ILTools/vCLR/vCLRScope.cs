2b512461bbb05ff2e6539633e1861431963c9486
Roman Alifanov <animaonline@gmail.com>
2013-11-04 11:55:06 +0100

    ILTools refactoring

--- LibAnimaonline/LibAnimaonline/ILTools/vCLR/vCLRScope.cs
+++ LibAnimaonline/LibAnimaonline/ILTools/vCLR/vCLRScope.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Animaonline.ILTools.vCLR
+﻿namespace Animaonline.ILTools.vCLR
 {
     public enum vCLRScope
     {
